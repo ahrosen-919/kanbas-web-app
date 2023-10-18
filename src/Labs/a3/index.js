e46af33fc563cbbd77767c08ae63a02d9e2082e3
Aidan Rosenberg <aidan@rosenbergs.org>
2023-10-17 21:28:15 -0400

    a3 finish lab

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -1,9 +1,19 @@
 import JavaScript from "./JavaScript";
+import Classes from "./Classes";
+import Styles from "./Styles";
+import ConditionalOutput from "./ConditionalOutput";
+import TodoItem from "./todo/TodoItem";
+import TodoList from "./todo/TodoList";
 
 function Assignment3() {
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <TodoList/>
+      <TodoItem/>
+      <ConditionalOutput/>
+      <Styles/>
+      <Classes/>
       <JavaScript/>
     </div>
   );
